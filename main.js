d3b7709511b625bddf44e9604dec5166dcae69c0
Botolog <81357028+Botolog@users.noreply.github.com>
2024-03-06 11:34:05 +0200

    tryinggapi

--- main.js
+++ main.js
@@ -12,9 +12,51 @@ const pathWithoutDomain = pathSegments.join("/");
 const pathArray = pathWithoutDomain.split("/");
 console.log("Path Array:", pathArray);
 
+function update_rout(act, size) {
+  fs.appendFileSync("rout.tes", `${act}${size}\n`);
+
+  if (act.toUpperCase() === "F") return `The car went Forward by ${size}`;
+  if (act.toUpperCase() === "B") return `The car went Backward by ${size}`;
+  if (act.toUpperCase() === "R") return `The car turned Right by ${size}`;
+  if (act.toUpperCase() === "L") return `The car turned Left by ${size}`;
+  if (act.toUpperCase() === "S") return `The Scanned the aria`;
+}
 
 if (pathArray[0]=="Y"){
   console.log('yoyomotherfckersitfuckingworking');
+}
+
+// Load the Google Drive API client library
+gapi.load('client', init);
+
+function init() {
+  // Initialize the API client library with your API key
+  gapi.client.init({
+    apiKey: 'YOUR_API_KEY',
+    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+    clientId: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/drive'
+  }).then(function() {
+    // Authenticate the user
+    return gapi.auth2.getAuthInstance().signIn();
+  }).then(function() {
+    // Once authenticated, you can make API requests
+    gapi.client.drive.files.list({
+      'pageSize': 10,
+      'fields': "nextPageToken, files(id, name)"
+    }).then(function(response) {
+      var files = response.result.files;
+      if (files && files.length > 0) {
+        console.log("Files:");
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          console.log(file.name + ' (' + file.id + ')');
+        }
+      } else {
+        console.log('No files found.');
+      }
+    });
+  });
 }
 
 // app.get('/US/:y/:arr', (req, res) => {
