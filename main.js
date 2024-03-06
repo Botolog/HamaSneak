4e6bb5ff09420f3beaa1442c9e00c8cfc5b5df4c
Botolog <81357028+Botolog@users.noreply.github.com>
2024-03-06 09:51:04 +0200

    ADDED NPM

---
+++ main.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 8080;
+
+const NUMBER_OF_ROWS = 180;
+
+app.get('/', (req, res) => {
+  res.send('yoyomotherfckersitfuckingworking');
+});
+
+app.get('/US/:y/:arr', (req, res) => {
+  const { y, arr } = req.params;
+  let files = fs.readdirSync('scans/');
+  let file_count = files.length;
+
+  let needCreate = true;
+  for (let file of files) {
+    if (file.startsWith("writing")) {
+      needCreate = false;
+      let data = fs.readFileSync(`scans/${file}`, 'utf8').split('\n');
+      data[y] = arr;
+      if (data[data.length - 1] === "") {
+        fs.writeFileSync(`scans/${file}`, data.join('\n'));
+      } else {
+        fs.unlinkSync(`scans/${file}`);
+        fs.writeFileSync(`scans/scan${file_count}.tes`, data.join('\n'));
+      }
+    }
+  }
+
+  if (needCreate) {
+    fs.writeFileSync(`scans/writing${file_count + 1}.tes`, ".\n".repeat(NUMBER_OF_ROWS - 1) + ".");
+    upload_scans(y, arr);
+  }
+
+  res.send(`got it! (Y=${y})`);
+});
+
+// Define other routes similarly...
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
