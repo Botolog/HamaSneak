7b7e9f738156bd4bcb8a397bc8bcb04f8a5f57fc
Botolog <81357028+Botolog@users.noreply.github.com>
2024-04-05 11:20:33 +0300

    i dont even know what

--- test/test2/test2.ino
+++ test/test2/test2.ino
@@ -1,42 +1,65 @@
 #include <WiFi.h>
-
-const char* ssid = "Kaluga 2.4";
-const char* password = "41931047";
-
+ 
+const char* ssid     = "Modin-Students";
+const char* password = "";
+ 
 WiFiServer server(80);
-
+ 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   
   // Connect to Wi-Fi
   Serial.println();
-  Serial.println("Connecting to WiFi");
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println("");
-  Serial.println("WiFi connected."); Serial.println(WiFi.localIP());
-
+  Serial.println("WiFi connected.");
+  
   // Start the server
   server.begin();
   Serial.println("Server started");
+  
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
-
+ 
 void loop() {
+  // Check if a client has connected
   WiFiClient client = server.available();
-  if (client) {
-    Serial.println("New client connected");
-    while (client.connected()) {
-      if (client.available()) {
-        String command = client.readStringUntil(';');
-        Serial.println("Received command: " + command);
-        // Process the command and control the car accordingly
-        // Example: controlMotors(command);
-      }
-    }
-    Serial.println("Client disconnected");
+  
+  if (!client) {
+    return;
   }
+  
+  // Wait until the client sends some data
+  Serial.println("New client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String request = client.readStringUntil(';');
+  Serial.println(request);
+  client.flush();
+  
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  // client.println("YELLO");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<h1>Hello from Arduino!</h1>");
+  client.println("</html>");
+  
+  delay(1);
+  Serial.println("Client disconnected");
 }
